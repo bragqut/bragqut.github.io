83938849b984e6a8784b5e1677856231cc0f54a4
Sam Clifford <sj.clifford@student.qut.edu.au>
2016-02-04 15:18:09 +1000

    replacing _plugins

---
+++ _plugins/page.rb
@@ -0,0 +1,8 @@
+module Jekyll
+  class Page
+  
+    def tags
+      (self.data['tags'] || '').split(',').map {|t| Tag.new(t)}
+    end
+  end
+end
---
+++ _plugins/site.rb
@@ -0,0 +1,17 @@
+module Jekyll
+  class Site
+
+  
+    def pages_by_tag
+      tag_ref = {}
+      self.pages.each do |page|
+        page.tags.each do |tag|
+          pages = tag_ref.fetch(tag, Set.new)
+          pages << page
+          tag_ref[tag] = pages
+        end
+      end
+      tag_ref
+    end
+  end
+end
